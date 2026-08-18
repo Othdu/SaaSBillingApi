bbed038517430c1c6688b4f42915865e87b1718d
Mohamed <moh.mus2019@gmail.com>
2026-08-18 17:01:55 +0300

    test: add unit tests for subscription state machine and proration service

---
+++ tests/SaaSBillingApi.UnitTests/ProrationServiceTests.cs
@@ -0,0 +1,82 @@
+﻿using SaaSBillingApi.Domain.Entities;
+using SaaSBillingApi.Domain.Services;
+using Xunit;
+
+namespace SaaSBillingApi.UnitTests;
+
+public class ProrationServiceTests
+{
+    private readonly ProrationService _sut = new();
+
+    [Fact]
+    public void UpgradeHalfwayThroughPeriod_ChargesHalfDifference()
+    {
+        var basicPlan = new Plan("Basic", 10m);
+        var proPlan = new Plan("Pro", 30m);
+
+        var periodStart = new DateTime(2026, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+        var periodEnd = new DateTime(2026, 1, 31, 0, 0, 0, DateTimeKind.Utc);
+        var changeDate = new DateTime(2026, 1, 16, 0, 0, 0, DateTimeKind.Utc);
+
+        var result = _sut.CalculateProratedAmount(basicPlan, proPlan, periodStart, periodEnd, changeDate);
+
+        Assert.Equal(10m, result);
+    }
+
+    [Fact]
+    public void DowngradeHalfwayThroughPeriod_CreditsCustomer()
+    {
+        var proPlan = new Plan("Pro", 30m);
+        var basicPlan = new Plan("Basic", 10m);
+
+        var periodStart = new DateTime(2026, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+        var periodEnd = new DateTime(2026, 1, 31, 0, 0, 0, DateTimeKind.Utc);
+        var changeDate = new DateTime(2026, 1, 16, 0, 0, 0, DateTimeKind.Utc);
+
+        var result = _sut.CalculateProratedAmount(proPlan, basicPlan, periodStart, periodEnd, changeDate);
+
+        Assert.Equal(-10m, result);
+    }
+
+    [Fact]
+    public void ChangeOnPeriodStart_ChargesFullDifference()
+    {
+        var basicPlan = new Plan("Basic", 10m);
+        var proPlan = new Plan("Pro", 30m);
+
+        var periodStart = new DateTime(2026, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+        var periodEnd = new DateTime(2026, 1, 31, 0, 0, 0, DateTimeKind.Utc);
+
+        var result = _sut.CalculateProratedAmount(basicPlan, proPlan, periodStart, periodEnd, periodStart);
+
+        Assert.Equal(20m, result);
+    }
+
+    [Fact]
+    public void ChangeOnPeriodEnd_ChargesNothing()
+    {
+        var basicPlan = new Plan("Basic", 10m);
+        var proPlan = new Plan("Pro", 30m);
+
+        var periodStart = new DateTime(2026, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+        var periodEnd = new DateTime(2026, 1, 31, 0, 0, 0, DateTimeKind.Utc);
+
+        var result = _sut.CalculateProratedAmount(basicPlan, proPlan, periodStart, periodEnd, periodEnd);
+
+        Assert.Equal(0m, result);
+    }
+
+    [Fact]
+    public void ChangeDateOutsidePeriod_Throws()
+    {
+        var basicPlan = new Plan("Basic", 10m);
+        var proPlan = new Plan("Pro", 30m);
+
+        var periodStart = new DateTime(2026, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+        var periodEnd = new DateTime(2026, 1, 31, 0, 0, 0, DateTimeKind.Utc);
+        var invalidChangeDate = periodEnd.AddDays(1);
+
+        Assert.Throws<ArgumentOutOfRangeException>(() =>
+            _sut.CalculateProratedAmount(basicPlan, proPlan, periodStart, periodEnd, invalidChangeDate));
+    }
+}
---
+++ tests/SaaSBillingApi.UnitTests/SubscriptionStateMachineTests.cs
@@ -0,0 +1,105 @@
+﻿using SaaSBillingApi.Domain.Entities;
+using SaaSBillingApi.Domain.Enums;
+using SaaSBillingApi.Domain.Exceptions;
+using Xunit;
+
+namespace SaaSBillingApi.UnitTests;
+
+public class SubscriptionStateMachineTests
+{
+    private static Subscription CreateTrialSubscription()
+    {
+        return new Subscription(
+            tenantId: Guid.NewGuid(),
+            planId: Guid.NewGuid(),
+            currentPeriodStartUtc: DateTime.UtcNow,
+            currentPeriodEndUtc: DateTime.UtcNow.AddDays(14));
+    }
+
+    [Fact]
+    public void NewSubscription_StartsInTrial()
+    {
+        var subscription = CreateTrialSubscription();
+
+        Assert.Equal(SubscriptionStatus.Trial, subscription.Status);
+    }
+
+    [Fact]
+    public void Activate_FromTrial_MovesToActive()
+    {
+        var subscription = CreateTrialSubscription();
+
+        subscription.Activate();
+
+        Assert.Equal(SubscriptionStatus.Active, subscription.Status);
+    }
+
+    [Fact]
+    public void Activate_FromPastDue_MovesToActive()
+    {
+        var subscription = CreateTrialSubscription();
+        subscription.Activate();
+        subscription.MarkPastDue();
+
+        subscription.Activate();
+
+        Assert.Equal(SubscriptionStatus.Active, subscription.Status);
+    }
+
+    [Fact]
+    public void Activate_FromCancelled_Throws()
+    {
+        var subscription = CreateTrialSubscription();
+        subscription.Cancel();
+
+        Assert.Throws<InvalidSubscriptionTransitionException>(() => subscription.Activate());
+    }
+
+    [Fact]
+    public void MarkPastDue_FromTrial_Throws()
+    {
+        var subscription = CreateTrialSubscription();
+
+        Assert.Throws<InvalidSubscriptionTransitionException>(() => subscription.MarkPastDue());
+    }
+
+    [Fact]
+    public void MarkPastDue_FromActive_MovesToPastDue()
+    {
+        var subscription = CreateTrialSubscription();
+        subscription.Activate();
+
+        subscription.MarkPastDue();
+
+        Assert.Equal(SubscriptionStatus.PastDue, subscription.Status);
+    }
+
+    [Fact]
+    public void Cancel_FromAnyNonCancelledState_MovesToCancelled()
+    {
+        var subscription = CreateTrialSubscription();
+
+        subscription.Cancel();
+
+        Assert.Equal(SubscriptionStatus.Cancelled, subscription.Status);
+        Assert.NotNull(subscription.CancelledAtUtc);
+    }
+
+    [Fact]
+    public void Cancel_WhenAlreadyCancelled_Throws()
+    {
+        var subscription = CreateTrialSubscription();
+        subscription.Cancel();
+
+        Assert.Throws<InvalidSubscriptionTransitionException>(() => subscription.Cancel());
+    }
+
+    [Fact]
+    public void ChangePlan_WhenCancelled_Throws()
+    {
+        var subscription = CreateTrialSubscription();
+        subscription.Cancel();
+
+        Assert.Throws<InvalidOperationException>(() => subscription.ChangePlan(Guid.NewGuid()));
+    }
+}
