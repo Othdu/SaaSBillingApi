6b5de0eb56ac2864c8ad4786ceeb73ffdf848b94
Mohamed <moh.mus2019@gmail.com>
2026-08-25 18:24:02 +0300

    feat: add User entity and UserRole enum to Domain

---
+++ src/SaaSBillingApi.Domain/Entities/User.cs
@@ -0,0 +1,35 @@
+﻿using SaaSBillingApi.Domain.Common;
+using SaaSBillingApi.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaaSBillingApi.Domain.Entities
+{
+    public class User :BaseEntity
+    {
+      public Guid TenantId { get; private set; }
+
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+       public UserRole Role { get; set; }
+        private User() { }
+        public User(Guid tenantId, string email, string passwordHash, UserRole role)
+        {
+            if (string.IsNullOrWhiteSpace(email))
+            {
+                throw new ArgumentException("Email cannot be null or empty.", nameof(email));
+            }
+            if (string.IsNullOrWhiteSpace(passwordHash))
+            {
+                throw new ArgumentException("Password hash cannot be null or empty.", nameof(passwordHash));
+            }
+            TenantId = tenantId;
+            Email = email.ToLowerInvariant(); 
+            PasswordHash = passwordHash;
+            Role = role;
+        }
+    }
+}
